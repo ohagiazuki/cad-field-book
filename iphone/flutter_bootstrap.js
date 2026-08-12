93fea6f8600000975a1e9903e659b04e9578f560
ohagiazuki <taizankawatani@gmail.com>
2026-08-12 13:39:45 +0900

    Force latest web assets to load

--- iphone/flutter_bootstrap.js
+++ iphone/flutter_bootstrap.js
@@ -33,7 +33,7 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06cd","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06cd","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=633cf1e-cache1"}]};
 
 
 _flutter.loader.load();
