2bb57612cc36bd8066f2b150ba60ab69d7f90007
ohagiazuki <taizankawatani@gmail.com>
2026-08-30 18:47:23 +0900

    Add configurable multi-column damage tables

--- iphone/flutter_bootstrap.js
+++ iphone/flutter_bootstrap.js
@@ -33,7 +33,16 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06cd","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06cd","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
+
+if (location.hostname === '127.0.0.1' || location.hostname === 'localhost') {
+  for (const build of _flutter.buildConfig.builds) {
+    if (build.mainJsPath && !build.mainJsPath.includes('?')) {
+      build.mainJsPath += '?v=20260824-iphone-webp30';
+    }
+  }
+}
 
 _flutter.loader.load();
+
